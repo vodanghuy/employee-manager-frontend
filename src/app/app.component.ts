2953b879d516523eb7aeaba72c6a0eddf39d0eae
Vo Dang Huy <iamdaghuy@gmail.com>
2025-04-20 19:52:31 +0700

    search employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,6 +77,24 @@ export class AppComponent implements OnInit{
     }
   }
 
+  public searchEmployee(key: string): void{
+    const result: Employee[] = [];
+    for (const employee of this.employees) {
+      if(employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ) 
+      {
+        result.push(employee);
+      }
+    }
+    this.employees = result;
+    if(result.length == 0 || !key){
+      this.getEmployees();
+    }
+  }
+
   public onOpenModal(employee: Employee | null, mode: string): void{
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
