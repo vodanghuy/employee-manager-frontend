dae87efa7809cfd3176724dc890903b068fa3216
Vo Dang Huy <iamdaghuy@gmail.com>
2025-04-20 18:29:33 +0700

    add employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -29,6 +30,21 @@ export class AppComponent implements OnInit{
       }
     )
   }
+
+  public onAddEmployee(addForm: NgForm): void{
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) =>{
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message)
+      }
+    );
+
+  }
+
   public onOpenModal(employee: Employee | null, mode: string): void{
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
